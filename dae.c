f2aee829b131616cfe2ec0bdaec68aab7856633d
Mike Klein <mtklein@gmail.com>
2025-07-20 11:16:34 -0500

    Refactor dae_step_euler (#6)
    * Factor Euler step into inner function
    
    * refine

--- dae.c
+++ dae.c
@@ -3,100 +3,81 @@
 #include <math.h>
 #include <stdlib.h>
 
-_Bool dae_step_euler(struct dae_system const *sys, double step, double *Y, double *Z, double t) {
-    int ny = sys->dim_y;
-    int nz = sys->dim_z;
+static _Bool step_euler(struct dae_system const *sys, double step,
+                        double *Y, double *Z, double t,
+                        double *dYdt, double *Y_new,
+                        double *Z_guess, double *Gvec,
+                        double *J, double *Delta) {
+    int const nY = sys->dim_y,
+              nZ = sys->dim_z;
 
-    double *dYdt = malloc((size_t)ny * sizeof *dYdt);
-    if (!dYdt) {
-        return (_Bool)0;
-    }
     sys->f(Y, Z, t, dYdt, sys->ctx);
-
-    double *Y_new = malloc((size_t)ny * sizeof *Y_new);
-    if (!Y_new) {
-        free(dYdt);
-        return (_Bool)0;
-    }
-    for (int i = 0; i < ny; ++i) {
+    for (int i = 0; i < nY; ++i) {
         Y_new[i] = Y[i] + step * dYdt[i];
     }
-
-    double *Z_guess = malloc((size_t)nz * sizeof *Z_guess);
-    if (!Z_guess) {
-        free(Y_new);
-        free(dYdt);
-        return (_Bool)0;
-    }
-    for (int i = 0; i < nz; ++i) {
+    for (int i = 0; i < nZ; ++i) {
         Z_guess[i] = Z[i];
     }
 
     for (int iter = 0; iter < 10; ++iter) {
-        double *Gvec = malloc((size_t)nz * sizeof *Gvec);
-        if (!Gvec) {
-            break;
-        }
         sys->g(Y_new, Z_guess, t + step, Gvec, sys->ctx);
         double norm = 0.0;
-        for (int i = 0; i < nz; ++i) {
+        for (int i = 0; i < nZ; ++i) {
             norm += Gvec[i] * Gvec[i];
         }
         if (norm < 1e-16) {
-            free(Gvec);
-            for (int i = 0; i < ny; ++i) {
+            for (int i = 0; i < nY; ++i) {
                 Y[i] = Y_new[i];
             }
-            for (int i = 0; i < nz; ++i) {
+            for (int i = 0; i < nZ; ++i) {
                 Z[i] = Z_guess[i];
             }
-            free(Z_guess);
-            free(Y_new);
-            free(dYdt);
             return (_Bool)1;
         }
-        size_t jm = (size_t)nz * (size_t)nz;
-        double *J = malloc(jm * sizeof *J);
-        double *Delta = malloc((size_t)nz * sizeof *Delta);
-        if (!J || !Delta) {
-            free(Delta);
-            free(J);
-            free(Gvec);
-            break;
-        }
         sys->gz(Y_new, Z_guess, t + step, J, sys->ctx);
-        for (int i = 0; i < nz; ++i) {
+        for (int i = 0; i < nZ; ++i) {
             Delta[i] = -Gvec[i];
         }
-        if (!lin_solve(J, Delta, nz)) {
-            free(Delta);
-            free(J);
-            free(Gvec);
+        if (!lin_solve(J, Delta, nZ)) {
             break;
         }
         double delta_norm = 0.0;
-        for (int i = 0; i < nz; ++i) {
+        for (int i = 0; i < nZ; ++i) {
             Z_guess[i] += Delta[i];
             delta_norm += Delta[i] * Delta[i];
         }
-        free(Delta);
-        free(J);
-        free(Gvec);
         if (delta_norm < 1e-16) {
-            for (int i = 0; i < ny; ++i) {
+            for (int i = 0; i < nY; ++i) {
                 Y[i] = Y_new[i];
             }
-            for (int i = 0; i < nz; ++i) {
+            for (int i = 0; i < nZ; ++i) {
                 Z[i] = Z_guess[i];
             }
-            free(Z_guess);
-            free(Y_new);
-            free(dYdt);
             return (_Bool)1;
         }
     }
-    free(Z_guess);
-    free(Y_new);
-    free(dYdt);
     return (_Bool)0;
+}
+
+_Bool dae_step_euler(struct dae_system const *sys, double step, double *Y, double *Z, double t) {
+    int const nY = sys->dim_y,
+              nZ = sys->dim_z,
+              nJ = nZ*nZ;
+
+    double *dYdt    = malloc((size_t)nY * sizeof(double)),
+           *Y_new   = malloc((size_t)nY * sizeof(double));
+    double *Z_guess = malloc((size_t)nZ * sizeof(double)),
+           *Gvec    = malloc((size_t)nZ * sizeof(double)),
+           *Delta   = malloc((size_t)nZ * sizeof(double));
+    double *J       = malloc((size_t)nJ * sizeof(double));
+
+    _Bool const ok = step_euler(sys, step, Y, Z, t, dYdt, Y_new, Z_guess, Gvec, J, Delta);
+
+    free(dYdt);
+    free(Y_new);
+    free(Z_guess);
+    free(Gvec);
+    free(J);
+    free(Delta);
+    return ok;
 }
